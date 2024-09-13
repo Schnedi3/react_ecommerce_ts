bed5ae19446a57256e392d8a9035be9cdb07877b
Schnedi3 <schnedi3@gmail.com>
2024-09-14 01:21:21 +0200

    Improve contact form

--- src/hooks/useValidationForm.ts
+++ src/hooks/useValidationForm.ts
@@ -5,13 +5,17 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
 
-const schema = zod.object({
-  name: zod.string().min(8, { message: "Must be more than 8 characters" }),
-  email: zod.string().email({ message: "Invalid email address" }),
+const contactSchema = zod.object({
+  name: zod
+    .string()
+    .min(1, "Field required")
+    .min(8, "Must be more than 8 characters"),
+  email: zod.string().min(1, "Field required").email("Invalid email address"),
   message: zod
     .string()
-    .min(10, { message: "Must be more than 10 characters" })
-    .max(80, { message: "Must be less than 50 characters" }),
+    .min(1, "Field required")
+    .min(10, "Must be more than 10 characters")
+    .max(50, "Must be less than 50 characters"),
 });
 
 export const useValidateForm = () => {
@@ -23,7 +27,7 @@ export const useValidateForm = () => {
     formState: { errors },
     reset,
   } = useForm<FormValues>({
-    resolver: zodResolver(schema),
+    resolver: zodResolver(contactSchema),
   });
 
   const onSubmit = (data: FormValues) => {
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,30 +9,52 @@ export const Contact = () => {
 
   return (
     <section className="container contact__container">
-      <>
-        <img className="contact__img" src={contactImage} alt="contact image" />
-        <form
-          className="contact__form"
-          onSubmit={handleSubmit(onSubmit)}
-          autoComplete="off"
-        >
-          <h2>Get in touch</h2>
-          <input type="text" placeholder="Name" {...register("name")} />
+      <img className="contact__img" src={contactImage} alt="contact image" />
+      <form
+        className="contact__form"
+        onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
+      >
+        <h2>Get in touch</h2>
+        <label htmlFor="name" className="label">
+          Name
           {errors.name && <span className="error">{errors.name.message}</span>}
-          <input type="email" placeholder="Email" {...register("email")} />
-          {errors.email && <span className="error">{errors.email.message}</span>}
+          <input
+            type="text"
+            id="name"
+            placeholder="John Doe"
+            {...register("name")}
+          />
+        </label>
+        <label htmlFor="email" className="label">
+          Email
+          <input
+            type="email"
+            id="email"
+            placeholder="johndoe@lorem.com"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="error">{errors.email.message}</span>
+          )}
+        </label>
+        <label htmlFor="message" className="label">
+          Message
+          {errors.message && (
+            <span className="error">{errors.message.message}</span>
+          )}
           <textarea
+            id="message"
             rows={3}
-            placeholder="Message"
+            placeholder="lorem ipsum..."
             {...register("message")}
           ></textarea>
-          {errors.message && <span className="error">{errors.message.message}</span>}
-          <button type="submit" className="contact__button">
-            send
-          </button>
-          {successMessage && <span className="success">{successMessage}</span>}
-        </form>
-      </>
+        </label>
+        <button type="submit" className="contact__button">
+          send
+        </button>
+        {successMessage && <span className="success">{successMessage}</span>}
+      </form>
     </section>
   );
 };
